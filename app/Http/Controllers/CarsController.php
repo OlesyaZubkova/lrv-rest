e821c0813ad74932483c6786204ffe3d4b050802
Olesya Zubkova <olesya_2802@mail.ru>
2022-12-27 21:55:40 +0300

    files added

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cars;
+use App\Http\Requests\CarsRequest;
+
+class CarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Cars::paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CarsRequest $request)
+    {
+        return Cars::create($request->validated());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Cars::findOfFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CarsRequest $request, Cars $cars)
+    {
+        $cars->fill($request->validated());
+        return $cars->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cars  $cars
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cars $cars)
+    {
+        if ($cars->delete()) {
+            return response(null, Responce::HTTP_NO_CONTENT);
+        }
+        return null;
+    }
+}
---
+++ app/Http/Requests/CarsRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+use Illuminate\Contracts\Validation\Validator;
+
+class CarsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'brand' => ['required', 'string'],
+            'model' => ['required', 'string'],
+            'price' => ['required', 'string'],
+        ];
+    }
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(
+            response($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
